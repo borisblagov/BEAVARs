29752d1e8a8a9210d3906e7953629b5473e52540
Boris Blagov <46528899+borisblagov@users.noreply.github.com>
2025-04-03 07:25:05 +0200

    first steps fore

--- main_Chan2020_BVAR_CSV.jl
+++ main_Chan2020_BVAR_CSV.jl
@@ -2,6 +2,8 @@ include("devPkgs.jl")
 using BEAVARs
 using DelimitedFiles
 using Plots
+using Parameters
+
 
 YY = readdlm("data/FRED_Chan2020_LBA.txt", ',');
 setup_str, hyper_str = makeSetup(YY,"Chan2020_LBA_csv")
@@ -13,11 +15,14 @@ IRF_median, IRF_68_low, IRF_68_high = irf_chol_overDraws_csv(store_beta,store_Σ
 # store_beta, store_Σ =  Chan2020_LBA_Minn(YY;hyp=hypChan2020,VARSetup = setup_str);
 # IRF_median, IRF_68_low, IRF_68_high = irf_chol_overDraws(store_beta,store_Σ,setup_str;shSize = "stdev");
 
-# i_var = 3;
-# plot(IRF_median[:,i_var,i_shock],ribbon = (IRF_median[:,i_var,i_shock].-IRF_68_low[:,i_var,1],IRF_68_high[:,i_var,i_shock].-IRF_median[:,i_var,1]),fillalpha=0.2,label="",title=var_names[i_var])
+i_var = 3;
+plot(IRF_median[:,i_var,i_shock],ribbon = (IRF_median[:,i_var,i_shock].-IRF_68_low[:,i_var,1],IRF_68_high[:,i_var,i_shock].-IRF_median[:,i_var,1]),fillalpha=0.2) # ,label="",title=var_names[i_var]
+
+@unpack n,p=setup_str
+A = reshape(store_beta[:,1],n*p+1,n)
 
 # n = setup_str.n
-# i_shock = 1;            # which variable you shock
+i_shock = 1;            # which variable you shock
 # plt = plot(layout=(3,2))
 #     for i_var = 1:n;
 #         plot!(plt,IRF_median[:,i_var,i_shock],ribbon = (IRF_median[:,i_var,i_shock].-IRF_68_low[:,i_var,1],IRF_68_high[:,i_var,i_shock].-IRF_median[:,i_var,1]),fillalpha=0.2,label="",title=var_names[i_var],titlefontsize=8,subplot=i_var)
