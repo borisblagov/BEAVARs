a3e9e7351d6d7a6d184412cbb37f68164edc8278
Boris Blagov <46528899+borisblagov@users.noreply.github.com>
2025-04-25 19:15:35 +0200

    adding H_B

--- main_CPZ2024.jl
+++ main_CPZ2024.jl
@@ -2,9 +2,16 @@ include("devPkgs.jl")
 using BEAVARs
 using DelimitedFiles
 using LinearAlgebra
+#using FillArrays
+using BlockArrays
+using BlockBandedMatrices
+using SparseArrays
 
+p = 2;
 n = 3;
-T = 12;
+T = 5;
+k = n*(p+1); kn = k*3
+Tf = n*T;
 YY = rand(T,n);
 YY[2,1] = NaN; YY[4,2] = NaN; YY[3,3] = NaN;
 
@@ -27,3 +34,32 @@ So = S_full[:,indL_non_wide]
 
 
 # Go = HB So ; Go yo = HB So yo = HB YY[So_bit]
+B0 = 1.0*I(3)
+B1 = rand(3,3)
+B2 = rand(3,3)*0.1
+
+blk = [B0;B1;B2]
+lin = LinearIndices(blk)
+
+
+bbm = BlockBandedMatrix(ones(Float64,Tf,Tf),n*ones(Int,T,),n*ones(Int,T,),(p,0))
+
+
+# create a matrix in the shape of H_B which hast the linear indices of the coefficient matrix. It will be used to select the coefficients
+bbm_lin = BlockBandedMatrix(ones(Int,Tf,Tf),n*ones(Int,T,),n*ones(Int,T,),(p,0))
+ 
+# this one iterates over the Blocks of the block-banded matrix bbm and fills them with the linear indices of the coefficients to generate the indices that select from the coefficient matrix 
+for ij = 0:p
+    for ii in 1:div(Tf,n)-ij
+        bbm_lin[Block(ii+ij,ii)]=lin[ 1 + (ij-0)*n : n + (ij-0)*n ,:]
+    end
+end
+
+
+blklin_ind = deepcopy(bbm_lin.data)
+
+
+# Initialize sparse H_B
+H_Bsp=sparse(bbm)
+H_Bsp.nzval[:] = @view blk[blklin_ind];
+
