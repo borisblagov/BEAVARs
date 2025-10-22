73cedd17442ddc3755aed562201d39fb0accdcb8
Boris Blagov <46528899+borisblagov@users.noreply.github.com>
2025-10-22 16:58:18 +0200

    .

--- BEAVARs_examples.jl
+++ BEAVARs_examples.jl
@@ -7,6 +7,7 @@ data = TimeArray(DateTime(2020,1,1):Quarter(1):DateTime(2027,4,1),rand(30,3))
 data_strct = BEAVARs.makeDataSetup(model_type,data)
 out_strct = beavar(model_type, set_strct, hyp_strct, data_strct)
 
+
 # Chan2020iniw
 model_type, set_strct, hyp_strct= makeSetup("Chan2020iniw",n_burn=20;n_save=50,p=2)
 data = TimeArray(DateTime(2020,1,1):Quarter(1):DateTime(2027,4,1),rand(30,3));
